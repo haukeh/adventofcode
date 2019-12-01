f5368a1958393849b33dd87ab5a9e66ed3ae1e3c
Hauke Hans <haukeh@posteo.de>
2019-12-01 12:49:40 +0100

    fmt

--- 2019/scala/src/main/scala/day1/Day1.scala
+++ 2019/scala/src/main/scala/day1/Day1.scala
@@ -5,10 +5,10 @@ import scala.io.Source
 object Day1 {
 
   def main(args: Array[String]): Unit = {
-		val p1 = partOne(input)
-		println(p1)
-		val p2 = partTwo(input)
-		println(p2)
+    val p1 = partOne(input)
+    println(p1)
+    val p2 = partTwo(input)
+    println(p2)
   }
 
   private def input = Source.fromResource("day1_1.txt").getLines().toSeq
@@ -21,17 +21,18 @@ object Day1 {
       }
   }
 
-  private def calcFuel(value: Double): Int = (Math.floor(value / 3.0d) - 2).toInt
+  private def calcFuel(value: Double): Int =
+    (Math.floor(value / 3.0d) - 2).toInt
 
   private def partTwo(lines: Seq[String]) = {
     lines
       .map(_.toDouble)
       .flatMap { line =>
-				def subcalc(fuel: Int): Seq[Int] = {
-					if (fuel <= 0) Nil
-					else subcalc(calcFuel(fuel.toDouble)) :+ fuel
-				}
-				subcalc(calcFuel(line))
+        def subcalc(fuel: Int): Seq[Int] = {
+          if (fuel <= 0) Nil
+          else subcalc(calcFuel(fuel.toDouble)) :+ fuel
+        }
+        subcalc(calcFuel(line))
       }
       .sum
   }
