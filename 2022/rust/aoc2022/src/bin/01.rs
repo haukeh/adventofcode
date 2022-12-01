8fa332086e80b92070f52455134019abf7bb0e62
Hauke Hans <haukeh@posteo.de>
2022-12-01 19:12:55 +0100

    remove unnecessary turbofish

--- 2022/rust/aoc2022/src/bin/01.rs
+++ 2022/rust/aoc2022/src/bin/01.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 fn main() {
     let elves = aoc2022::input_as_string(1)
         .split("\n\n")
-        .map(|l| l.lines().map(|i| i.parse::<u32>().unwrap()).sum::<u32>())
+        .map(|l| l.lines().map(|i| i.parse::<u32>().unwrap()).sum())
         .sorted()
         .rev()
         .collect_vec();
