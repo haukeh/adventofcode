deecba850c2a546f7edd04ffa0b576ea993f2314
Hauke Hans <haukeh@posteo.de>
2022-12-12 15:37:50 +0100

    finish d12

--- 2022/python/12.py
+++ 2022/python/12.py
@@ -1,5 +1,5 @@
-from collections import deque
 import sys
+from collections import deque
 
 
 def transform(c):
@@ -10,8 +10,7 @@ def transform(c):
     else:
         return ord(c) - ord('a') + 1
 
-
-G = [[x for x in y] for y in open("input/12_t").read().split("\n")]
+G = [[x for x in y] for y in open("input/12").read().split("\n")]
 
 for y in range(len(G)):
     for x in range(len(G[0])):
@@ -22,32 +21,38 @@ for y in range(len(G)):
 
         G[y][x] = transform(G[y][x])
 
-for y in G: 
-    print(f'{str(y)}\n')
+def shortest_path(start):
+    global G
+    
+    q = deque([(start[0], start[1], 0)])
+    visited = set()
 
-shortest = sys.maxsize
-visited = set()
+    while q:
+        x, y, l = q.popleft()
+        if (x, y) == E:
+            return l
+        
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
 
-Q = deque([(S[0], S[1], 1)])
-while Q:
-    x, y, l = Q.popleft()
+        for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            yy = y + dy
+            xx = x + dx        
+            if (0 <= yy < len(G)) and (0 <= xx < len(G[0])):
+                if (G[yy][xx] <= G[y][x]+1):
+                    q.append((xx, yy, l + 1))
+    
 
-    print(f'{x} {y} {l}')
+starts = []
+for y in range(len(G)):
+    for x in range(len(G[0])):
+        if G[y][x] == 1:
+            starts.append((x, y))
 
-    if (x, y) == E:
-        print("Found E")
-        shortest = l
-        break
+p1 = shortest_path(S)
+p2 = min(filter(lambda x: x, [shortest_path(s) for s in starts]))
 
-    if (x, y) in visited:
-        continue
-    visited.add((x, y))
+print(p1)
+print(p2)
 
-    for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-        nx = x + dx
-        ny = y + dy
-        if (0 <= ny < len(G)) and (0 <= nx < len(G[0])):
-            if (G[ny][nx] + 1 <= G[y][x]):
-                Q.append((nx, ny, l + 1))
-
-print(shortest)
