e8a2c6c58008777250338316fee95aa2f2864d53
Hauke Hans <haukeh@posteo.de>
2024-12-02 16:50:45 +0100

    clean up day2

--- src/bin/d2.rs
+++ src/bin/d2.rs
@@ -1,9 +1,5 @@
 use aoc2024::file_lines;
-
-enum Ordering {
-    ASC,
-    DESC,
-}
+use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
     let lines = file_lines("input/d2.txt")?;
@@ -23,18 +19,13 @@ fn main() -> anyhow::Result<()> {
 
     let p2 = reports
         .iter()
-        .filter(|report| {
-            let combinations: Vec<_> = (0..report.len())
-                .map(|n| {
-                    report[..n]
-                        .iter()
-                        .chain(report[n + 1..].iter())
-                        .map(|&n| n)
-                        .collect::<Vec<_>>()
+        .filter(|&report| {
+            check_report(report)
+                || (0..report.len()).any(|n| {
+                    let mut r = report.clone();
+                    r.remove(n);
+                    check_report(&r)
                 })
-                .collect();
-
-            check_report(report) || combinations.iter().filter(|&r| check_report(r)).count() > 0
         })
         .count();
 
@@ -45,40 +36,12 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn check_report(report: &[usize]) -> bool {
-    let mut iter = report.iter().peekable();
-    let mut ordering: Option<Ordering> = None;
-
-    while let Some(current) = iter.next() {
-        if let Some(&next) = iter.peek() {
-            match ordering {
-                Some(Ordering::ASC) => {
-                    if next < current || !allowed_diff(*current, *next) {
-                        return false;
-                    }
-                }
-                Some(Ordering::DESC) => {
-                    if next > current || !allowed_diff(*current, *next) {
-                        return false;
-                    }
-                }
-                None => {
-                    ordering = if next > current {
-                        Some(Ordering::ASC)
-                    } else {
-                        Some(Ordering::DESC)
-                    };
-                    if !allowed_diff(*current, *next) {
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-
-    return true;
+    let pairs: Vec<(&usize, &usize)> = report.iter().tuple_windows().collect();
+    return (pairs.iter().all(|(a, b)| a > b) || pairs.iter().all(|(a, b)| a < b))
+        && pairs.iter().all(|(&a, &b)| has_allowed_diff(a, b));
 }
 
-fn allowed_diff(a: usize, b: usize) -> bool {
+fn has_allowed_diff(a: usize, b: usize) -> bool {
     let diff = a.abs_diff(b);
     return diff >= 1 && diff <= 3;
 }
