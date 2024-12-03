855549af0a9098cac6aabc84a7a23924ad89e008
Hauke Hans <haukeh@posteo.de>
2024-12-03 12:04:37 +0100

    day3 initial

---
+++ src/bin/d3.rs
@@ -0,0 +1,55 @@
+use aoc2024::file_as_string;
+use regex::{Captures, Regex};
+
+enum Token {
+    Do,
+    Dont,
+    Mul(i64, i64),
+}
+
+impl Token {
+    pub fn parse(capture: Captures<'_>) -> Self {
+        match capture.get(0).unwrap().as_str() {
+            "do()" => Self::Do,
+            "don't()" => Self::Dont,
+            _ => {
+                let left_op: i64 = capture.get(1).map(|n| n.as_str().parse().unwrap()).unwrap();
+                let right_op: i64 = capture.get(2).map(|n| n.as_str().parse().unwrap()).unwrap();
+                Self::Mul(left_op, right_op)
+            }
+        }
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let text = file_as_string("input/d3.txt")?;
+    let r = Regex::new(r"don't\(\)|do\(\)|mul\((\d+)\,(\d+)\)")?;
+    let tokens: Vec<Token> = r.captures_iter(&text).map(|c| Token::parse(c)).collect();
+
+    let p1 = tokens
+        .iter()
+        .filter_map(|tok| match tok {
+            Token::Mul(a, b) => Some(a * b),
+            _ => None,
+        })
+        .sum::<i64>();
+
+    let mut p2: i64 = 0;
+    let mut on: bool = true;
+    for t in tokens.iter() {
+        match t {
+            Token::Do => on = true,
+            Token::Dont => on = false,
+            Token::Mul(a, b) => {
+                if on {
+                    p2 += a * b
+                }
+            }
+        }
+    }
+
+    println!("{}", p1);
+    println!("{}", p2);
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
@@ -8,4 +8,10 @@ pub fn file_lines(filename: &str) -> io::Result<impl Iterator<Item = String>> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
     Ok(reader.lines().map(|l| l.unwrap()))
+}
+
+pub fn file_as_string(filename: &str) -> io::Result<String> {
+    let project_dir = env!("CARGO_MANIFEST_DIR");
+    let file_path = Path::new(project_dir).join(filename);
+    return fs::read_to_string(file_path);
 }
