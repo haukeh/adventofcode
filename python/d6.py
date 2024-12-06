2154af003ef80fb7c2873c936ba5a541f9ad8866
Hauke Hans <haukeh@posteo.de>
2024-12-06 17:26:02 +0100

    simplify d6

--- python/d6.py
+++ python/d6.py
@@ -1,58 +1,52 @@
-lines = [list(line.strip()) for line in open("../input/d6.txt").readlines()]
-grid = {(x, y): c for y, xs in enumerate(lines) for x, c in enumerate(xs)}
-start = [key for key, value in grid.items() if value == "^"][0]
-
-
-def p1():
+def sim_guard(grid, start):
     visited = set()
     pos = start
     dir = (0, -1)
+    loop = False
+
     while pos in grid:
-        visited.add(pos)
-        next = (pos[0] + dir[0], pos[1] + dir[1])
-        if not next:
+        if (pos, dir) in visited:
+            loop = True
             break
 
+        visited.add((pos, dir))
+        
+        next = (pos[0] + dir[0], pos[1] + dir[1])
         item = grid.get(next)
+        if item is None:
+            break
+
         if item == "#":
             dx, dy = dir
             dir = (-dy, dx)
-
-        pos = (pos[0] + dir[0], pos[1] + dir[1])
-
-    print(len(visited))
-
-
-def p2():
-    loops = 0    
-
-    for obs, v in grid.items():
-        visited = set()        
-        pos = start                
-        dir = (0, -1)
-        if v == ".":                        
-            while pos in grid:
-                if (pos, dir) in visited:                    
-                    loops += 1
-                    break
-
-                visited.add((pos, dir))
-
-                next = (pos[0] + dir[0], pos[1] + dir[1])                
-                item = grid.get(next)
-                if item is None:                    
-                    break
-                
-                if item == "#" or next == obs:
-                    dx, dy = dir
-                    dir = (-dy, dx)                
-                else:
-                    pos = next
         else:
-            continue
-        
-    print(loops)
+            pos = next
+
+    return set(map(lambda p: p[0], visited)), loop
 
 
-p1()
-p2()
+grid = {
+    (x, y): char
+    for y, line in enumerate(open("../input/d6.txt").readlines())
+    for x, char in enumerate(line.strip())
+}
+
+start = next(key for key, value in grid.items() if value == "^")
+guard_path, _ = sim_guard(grid, start)
+
+p1 = len(guard_path)
+p2 = 0
+for pos in guard_path:
+    if grid[pos] == ".":
+        grid[pos] = "#"
+
+        _, loop = sim_guard(grid, start)
+
+        if loop:
+            p2 += 1
+
+        grid[pos] = "."
+
+
+print(p1)
+print(p2)
